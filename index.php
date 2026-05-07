c02fde07e8a733406906155fbf303ab9cc2fc678
Souzatitan <asouzanderson@gmail.com>
2026-05-06 23:47:08 -0300

    enable php rrors

--- index.php
+++ index.php
@@ -8,3 +8,30 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use App\Core\Router;
+use App\Controllers\AdminController;
+
+$router = new Router();
+
+/*
+|--------------------------------------------------------------------------
+| ROTAS
+|--------------------------------------------------------------------------
+*/
+
+$router->add('POST', '/admin/login', [AdminController::class, 'login']);
+
+/*
+|--------------------------------------------------------------------------
+| EXECUTA
+|--------------------------------------------------------------------------
+*/
+
+$router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
