08f991892e461ee36ca7df420f50ddf6f9db292f
Souzatitan <asouzanderson@gmail.com>
2026-05-06 23:31:59 -0300

    enable php errors

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-?>
